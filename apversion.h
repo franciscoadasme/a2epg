7ad2870ea730169b1e7f4290765336c8eaf620d1
Francisco Adasme <francisco.adasme@gmail.com>
2014-04-30 21:32:35 -0400

    Bumped version

--- apversion.h
+++ apversion.h
@@ -1,8 +1,8 @@
 #ifndef APVERSION_H
 #define APVERSION_H
 
-#define AP_VERSION "0.9.7"
-#define AP_BUILD 108
+#define AP_VERSION "0.9.8"
+#define AP_BUILD 114
 #define AP_BUILD_DATE "Apr 2014"
 
 #endif // APVERSION_H
